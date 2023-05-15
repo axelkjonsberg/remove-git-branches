f8d16badfb525fa5727f63f17ee412d5e859bf1f
Axel Kjønsberg <axelkjonsberg@gmail.com>
2023-05-15 14:38:24 +0200

    PowerShell: Refactor code

--- Powershell/Remove-GitBranches.ps1
+++ Powershell/Remove-GitBranches.ps1
@@ -20,81 +20,44 @@ function Remove-GitBranches {
     # Fetch and prune
     git fetch -p
 
-    # Get current branch
-    $currentBranch = git symbolic-ref --short -q HEAD
+    $currentBranch = (git symbolic-ref --short -q HEAD).Trim()
 
-    $masterBranch = ''
-    if ($DeleteCurrent) {
-        if (branchExists 'master') {
-            $masterBranch = 'master'
-        }
-
-        if (branchExists 'main') {
-            $masterBranch = 'main'
-        }
-
-        if ($masterBranch -eq '') {
+    $masterBranch = if ($DeleteCurrent) {
+        if (branchExists 'master') { 'master' }
+        elseif (branchExists 'main') { 'main' }
+        else {
             Write-Host "Neither 'main' nor 'master' branches exist. Cannot switch branches after deleting current." -ForegroundColor Red
             return
         }
     }
 
-    $shouldSwitchBranch = $DeleteCurrent -and $currentBranch -ne $masterBranch
+    $localBranches = (git branch).ForEach({ $_.Trim().Replace('* ', '') })
+    $remoteBranches = (git branch -r).ForEach({ $_.Trim().Replace('origin/', '') })
 
-    # Get a list of local branches
-    $localBranches = git branch | ForEach-Object {
-        # Remove the asterisk prefix if present and trim white spaces
-        $branchName = $_.Trim().Replace('* ', '')
-        return $branchName
-    }
+    $branchesToDelete = $localBranches | Where-Object { $_ -notin $remoteBranches -and $_ -ne 'master' -and $_ -ne 'main' -and (($_ -ne $currentBranch) -or $DeleteCurrent) }
 
-    # Get a list of remote branches
-    $remoteBranches = git branch -r | ForEach-Object {
-        # Remove the "origin/" prefix and trim white spaces
-        $branchName = $_.Trim().Replace('origin/', '')
-        return $branchName
-    }
-
-    # Get a list of branches to delete
-    $branchesToDelete = $localBranches | Where-Object { $branchName = $_; -not ($remoteBranches -contains $branchName) } | ForEach-Object {
-        # Skip master and main branches
-        if ($branchName -eq 'master' -or $branchName -eq 'main') {
-            return
-        }
-        
-        # Skip current branch if DeleteCurrent flag is not true
-        if ($branchName -eq $currentBranch -and !$DeleteCurrent) {
-            return
-        }
-
-        # Add branch to list
-        return $branchName
-    }
-
-    # If there are no branches to delete, return
-    if ($branchesToDelete.Count -eq 0) {
+    if (-not $branchesToDelete) {
         Write-Host "No branches to delete" -ForegroundColor Cyan
         return
     }
 
-    # Display branches to delete and prompt for confirmation
     Write-Host "The following local Git branches will be deleted:" -ForegroundColor Yellow
     $branchesToDelete | ForEach-Object { Write-Host "  - $_" -ForegroundColor Magenta }
-    $confirmation = Read-Host "Are you sure you want to delete these branches? (y/N)"
     
-    if ($confirmation -ne 'y') {
+    if ((Read-Host "Are you sure you want to delete these branches? (y/N)") -ne 'y') {
         Write-Host "No branches were deleted" -ForegroundColor Cyan
         return
     }
     
-    # If the DeleteCurrent flag is true, and current branch is equal to master branch, stash changes, switch to main or master, and pop and apply stash
-    if ($shouldSwitchBranch) {
+    # Switch branch if current should be deleted and current is not master branch
+    if ($DeleteCurrent -and $currentBranch -ne $masterBranch) {
         git checkout $masterBranch
     }
 
     $branchesToDelete | ForEach-Object {
         git branch -D $_
     }
+
     Write-Host "The branches were deleted successfully" -ForegroundColor Green
 }
 

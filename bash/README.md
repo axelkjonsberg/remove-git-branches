74e9d70a36086c9874df872b723c706de751d494
Axel Kjønsberg <axelkjonsberg@gmail.com>
2023-05-16 09:27:26 +0200

    Fix Bash script and how-to

--- bash/README.md
+++ bash/README.md
@@ -1,10 +1,17 @@
-# How to use
+# RGB for Bash
 
-Place this script anywhere and add an alias to it in your .bashrc or .zshrc:
+Place this script anywhere and add an alias to it in your _.bashrc_ or _.zshrc_:
 
 ```bash
-alias clean_orphaned_branches='$PATH/remove-git-branches.sh'
+alias remove_git_branches='absolute/path/to/remove-git-branches.sh'
+alias rgb='absolute/path/to/remove-git-branches.sh'
 ```
+
+If you are using Git Bash, Cygwin, or Windows Subsystem for Linux (WSL) on Windows, you would place your .bashrc file in your home directory.
+
+**Git Bash** or **Cygwin** do not create a .bashrc file by default; create this file yourself if it does not already exist. This can be done with `touch ~/.bashrc` in a new terminal.
+
+In **WSL**, the .bashrc file should be located in your home directory (/home/\<username>/.bashrc).
 
 If you get a permission error, enable execution of the script with:
 
--- bash/remove-git-branches.sh
+++ bash/remove-git-branches.sh
@@ -1,71 +1,81 @@
 #!/bin/bash
 
 function branch-exists {
-  git show-ref --verify --quiet refs/heads/$1
+    git show-ref --verify --quiet refs/heads/$1
 }
 
 function remove-git-branches {
-  delete_current=$1
+    delete_current=$1
 
-  is_inside_work_tree=$(git rev-parse --is-inside-work-tree)
+    is_inside_work_tree=$(git rev-parse --is-inside-work-tree)
 
-  if [[ ! $is_inside_work_tree ]]; then
-    printf "\e[0;31mNot inside a Git repository\e[0m\n"
-    exit 1
-  fi
-
-  git fetch -p
-
-  current_branch=$(git symbolic-ref --short -q HEAD | xargs)
-  
-  if branch-exists 'master'; then
-    default_branch='master'
-  elif branch-exists 'main'; then
-    default_branch='main'
-  fi
-  
-  if [[ -z "$delete_current" && -n "$default_branch" && "$current_branch" != "$default_branch" ]]; then
-    read -p "Also delete current branch $current_branch and switch to $default_branch? (y/N): " choice
-    if [[ "$choice" =~ ^[Yy]$ ]]; then
-      delete_current=true
+    if [[ ! $is_inside_work_tree ]]; then
+        printf "\e[0;31mNot inside a Git repository\e[0m\n"
+        exit 1
     fi
-  fi
 
-  if [[ -n "$delete_current" && "$current_branch" != "$default_branch" && -z "$default_branch" ]]; then
-    printf "\e[0;31mNeither 'main' nor 'master' branches exist. Cannot switch branches after deleting current.\e[0m\n"
-    exit 1
-  fi
+    git fetch -p
 
-  local_branches=$(git branch | sed 's/\* //' | xargs)
-  remote_branches=$(git branch -r | sed 's/origin\///g' | sed 's/HEAD -> //' | sort -u | xargs)
+    current_branch=$(git symbolic-ref --short -q HEAD | xargs)
 
-  branches_to_delete=$(echo ${local_branches[@]} ${remote_branches[@]} | sed 's/ /\n/g' | sort | uniq -u)
+    if branch-exists 'master'; then
+        default_branch='master'
+    elif branch-exists 'main'; then
+        default_branch='main'
+    fi
 
-  if [[ -z "$branches_to_delete" ]]; then
-    printf "\e[0;36mNo branches to delete\e[0m\n"
-    exit 0
-  fi
+    if [[ -z "$delete_current" && -n "$default_branch" && "$current_branch" != "$default_branch" ]]; then
+        read -p "Also delete current branch $current_branch and switch to $default_branch? (y/N): " choice
+        if [[ "$choice" =~ ^[Yy]$ ]]; then
+            delete_current=true
+        fi
+    fi
 
-  printf "\e[0;33mThe following local Git branches will be deleted:\e[0m\n"
+    if [[ -n "$delete_current" && "$current_branch" != "$default_branch" && -z "$default_branch" ]]; then
+        printf "\e[0;31mNeither 'main' nor 'master' branches exist. Cannot switch branches after deleting current.\e[0m\n"
+        exit 1
+    fi
 
-  echo "$branches_to_delete" | tr ' ' '\n'
+    local_branches=($(git branch | sed 's/\* //'))
 
-  read -p "Are you sure you want to delete these branches? (y/N): " choice
+    remote_branches=($(git branch -r | sed 's/origin\///g' | sed 's/HEAD -> //' | sort -u))
 
-  if [[ ! "$choice" =~ ^[Yy]$ ]]; then
-    printf "\e[0;36mNo branches to delete\e[0m\n"
-    exit 0
-  fi
+    branches_to_delete=()
+    for branch in "${local_branches[@]}"; do
+        if [[ ! " ${remote_branches[*]} " =~ " ${branch} " ]] && \
+            [ "$branch" != "master" ] && [ "$branch" != "main" ] && \
+            { [ "$branch" != "$current_branch" ] || [ "$delete_current" = true ]; }; then
+            branches_to_delete+=("$branch")
+        fi
+    done
 
-  if [[ -n "$delete_current" && "$current_branch" != "$default_branch" ]]; then
-    git switch $default_branch
-  fi
+    if [[ -z "$branches_to_delete" ]]; then
+        printf "\e[0;36mNo branches to delete\e[0m\n"
+        exit 0
+    fi
 
-  for branch in $branches_to_delete; do
-    git branch -D $branch
-  done
+    printf "\e[0;33mThe following local Git branches will be deleted:\e[0m\n"
 
-  printf "\e[0;32mThe branches were deleted successfully\e[0m\n"
+    for branch in "${branches_to_delete[@]}"; do
+        echo -e "\e[35m  - $branch\e[0m"
+    done
+
+    read -p "Are you sure you want to delete these branches? (y/N): " choice
+
+    if [[ ! "$choice" =~ ^[Yy]$ ]]; then
+        printf "\e[0;36mNo branches were deleted\e[0m\n"
+        exit 0
+    fi
+
+    if [[ -n "$delete_current" && "$current_branch" != "$default_branch" ]]; then
+        git switch $default_branch
+    fi
+
+    for branch in $branches_to_delete; do
+        git branch -D $branch
+    done
+
+    printf "\e[0;32mThe branches were deleted successfully\e[0m\n"
 }
 
 remove-git-branches $1
